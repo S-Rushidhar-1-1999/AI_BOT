7bc5855ce04aa0737f69c43c881481f53d7584a2
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-05-18 22:39:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', '19491592'))
-API_HASH = environ.get('API_HASH', '01a4118f7aec3b2caece77a057fdd197')
-BOT_TOKEN = environ.get('BOT_TOKEN', "5759842612:AAE0gN_o90wAesiMuPE9oPsPS34cCn7P34Q")
+api_id = int(environ.get('API_ID', '19491592'))
+api_hash = environ.get('API_HASH', '01a4118f7aec3b2caece77a057fdd197')
+bot_token = environ.get('BOT_TOKEN', "5759842612:AAE0gN_o90wAesiMuPE9oPsPS34cCn7P34Q")
 
 OWNER_USERNAME  = environ.get("OWNER_USERNAME","Rushidhar1999")  # WITHOUT @
 UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL","USE_FULL_BOTZ")  # WITHOUT @
