fc21eb8b1eeeab402e828171587780be46e557fa
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-05-19 00:40:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,7 +55,6 @@ async def answer(bot, message):
     if True: 
         user_id = message.from_user.id
         if user_id:
-            print("ok")
             try:
                 message = message.text
                 user_id = message.from_user.id
@@ -68,12 +67,10 @@ async def answer(bot, message):
                     frequency_penalty=0.1,
                     presence_penalty = 0.0,
                 )
-                reply_markup=InlineKeyboardMarkup(btn)
                 footer_credit = f"Join My Updates Channel 🦋 @{UPDATES_CHANNEL} 🦋 \nOwner 🦋 @{OWNER_USERNAME} 🦋"
                 response = response.choices[0].text 
                 await bot.send_message(AI_LOGS, text=f"⚡️⚡️#AI_Query \n\n• A user named **{message.from_user.mention}** with user id - `{user_id}`. Asked me this query...\n\n══❚█══Q   U   E   R   Y══█❚══\n\n\n[Q྿.]**{message}**\n\n👇Here is what i responded:\n:-`{response}`\n\n\n❚═USER ID═❚═• `{user_id}` \n❚═USER Name═❚═• `{message.from_user.mention}` \n\n🗃️")
                 await message.reply(f"{response}\n\n\n{footer_credit}")
-                print("nice")
             except Exception as error:
                 print(error)
     else:
