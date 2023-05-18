e0c2bd41d8ea777c0933ea2756580c20c38aac92
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-05-18 23:29:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ bot_token = environ.get('BOT_TOKEN', "5759842612:AAE0gN_o90wAesiMuPE9oPsPS34cCn7
 OWNER_USERNAME  = environ.get("OWNER_USERNAME","Rushidhar1999")  # WITHOUT @
 UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL","USE_FULL_BOTZ")  # WITHOUT @
 
-OPENAI_API = environ.get("OPENAI_API","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")
+OPENAI_API = environ.get("OPENAI_API","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")
 AI_LOGS = int(environ.get("AI_LOGS","-1001892393019"))
