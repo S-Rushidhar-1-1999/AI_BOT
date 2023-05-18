1f5f97164760e0bc404945e58d655944d172c24e
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-05-18 22:18:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ from os import environ
 
 API_ID = int(environ.get('API_ID', '19491592'))
 API_HASH = environ.get('API_HASH', '01a4118f7aec3b2caece77a057fdd197')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "5759842612:AAE0gN_o90wAesiMuPE9oPsPS34cCn7P34Q")
 
-OWNER_USERNAME  = environ.get("OWNER_USERNAME","")  # WITHOUT @
-UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL","")  # WITHOUT @
+OWNER_USERNAME  = environ.get("OWNER_USERNAME","Rushidhar1999")  # WITHOUT @
+UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL","USE_FULL_BOTZ")  # WITHOUT @
 
 OPENAI_API = environ.get("OPENAI_API","")
-AI_LOGS = int(environ.get("AI_LOGS",""))
+AI_LOGS = int(environ.get("AI_LOGS","-1001892393019"))
